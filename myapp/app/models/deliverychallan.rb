0c6402b8b82920dbcf5028b3edbd23600ea31d5d
Hitesh <pradeep.narava@gmail.com>
2010-05-02 00:16:42 +0530

    DC

--- myapp/app/models/deliverychallan.rb
+++ myapp/app/models/deliverychallan.rb
@@ -1,4 +1,5 @@
 class Deliverychallan < ActiveRecord::Base
- belongs_to:customer
- belongs_to:customer_order
+ belongs_to :customer
+ belongs_to :customer_order
+ validates_presence_of :cst_tin
 end
