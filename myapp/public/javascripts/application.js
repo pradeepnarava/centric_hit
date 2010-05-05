2a00a14244aeaab1c5d6a703e53c64f4d0006422
unknown <Santosh@.(none)>
2010-05-05 20:23:32 +0530

    ui changes

--- myapp/public/javascripts/application.js
+++ myapp/public/javascripts/application.js
@@ -65,82 +65,82 @@ function customer_po(id){
 }
 
 function total_price(row){ 
-    rpkg  = jQuery("#"+row+"_customer_order_rate_per_kilo").val();
-    twt  = jQuery("#"+row+"_customer_order_rate_pcs").val();
+    var rpkg  = jQuery("#"+row+"_customer_order_rate_per_kilo").val();
+    var twt  = jQuery("#"+row+"_customer_order_rate_pcs").val();
     jQuery("#"+row+"_customer_order_total_amount").val(rpkg*twt);
 }
 
 function total_pcs_mts(row){    
-    length= jQuery("#"+row+"_customer_order_h_length").val();
-    od  = jQuery("#"+row+"_customer_order_h_od").val();
-    mtr  = jQuery("#"+row+"_customer_order_rate_mtr").val();
-    thikness  = jQuery("#"+row+"_customer_order_h_thikness").val();
-    i  = ((od-thikness)*thikness)/40549;  
+    var length= jQuery("#"+row+"_customer_order_h_length").val();
+    var od  = jQuery("#"+row+"_customer_order_h_od").val();
+    var mtr  = jQuery("#"+row+"_customer_order_rate_mtr").val();
+    var thikness  = jQuery("#"+row+"_customer_order_h_thikness").val();
+    var i  = ((od-thikness)*thikness)/40549;  
     jQuery("#"+row+"_customer_order_rate_pcs").val(Number(mtr*length).toFixed(3));
     jQuery("#"+row+"_customer_order_rate_mt").val(Number(mtr/i).toFixed(3));
     
 }
 
 function total_mtr_mts(row){    
-    pcs  = jQuery("#"+row+"_customer_order_rate_pcs").val();
-    length= jQuery("#"+row+"_customer_order_h_length").val();
-    od  = jQuery("#"+row+"_customer_order_h_od").val();
-    thikness  = jQuery("#"+row+"_customer_order_h_thikness").val();
-    i  = ((od-thikness)*thikness)/40549;
+    var pcs  = jQuery("#"+row+"_customer_order_rate_pcs").val();
+    var length= jQuery("#"+row+"_customer_order_h_length").val();
+    var od  = jQuery("#"+row+"_customer_order_h_od").val();
+    var thikness  = jQuery("#"+row+"_customer_order_h_thikness").val();
+    var i  = ((od-thikness)*thikness)/40549;
     jQuery("#"+row+"_customer_order_rate_mt").val(Number(pcs/(length*i)).toFixed(3));
     jQuery("#"+row+"_customer_order_rate_mtr").val(Number(pcs/length).toFixed(3));
 }
 
 
 function total_mtr_pcs(row){    
-    mtonnes  = jQuery("#"+row+"_customer_order_rate_mt").val();
-    length= jQuery("#"+row+"_customer_order_h_length").val();
-    od  = jQuery("#"+row+"_customer_order_h_od").val();
-    thikness  = jQuery("#"+row+"_customer_order_h_thikness").val();
-    i  = ((od-thikness)*thikness)/40549;
+    var mtonnes  = jQuery("#"+row+"_customer_order_rate_mt").val();
+    var length= jQuery("#"+row+"_customer_order_h_length").val();
+    var od  = jQuery("#"+row+"_customer_order_h_od").val();
+    var thikness  = jQuery("#"+row+"_customer_order_h_thikness").val();
+    var i  = ((od-thikness)*thikness)/40549;
     jQuery("#"+row+"_customer_order_rate_mtr").val(Number(((od-thikness)*thikness*0.2465*mtonnes)/10000).toFixed(3));
     jQuery("#"+row+"_customer_order_rate_pcs").val(Number(i*mtonnes*length).toFixed(3));
 }
 
 
 function total_weight(row){
-    quantity  = jQuery("#"+row+"_customer_order_quantity").val();
-    length= jQuery("#"+row+"_customer_order_h_length").val();
-    od  = jQuery("#"+row+"_customer_order_h_od").val();
-    thikness  = jQuery("#"+row+"_customer_order_h_thikness").val();
-    i  = ((od-thikness)*thikness*length*0.02465*quantity)/1000;
+    var quantity  = jQuery("#"+row+"_customer_order_quantity").val();
+    var length= jQuery("#"+row+"_customer_order_h_length").val();
+    var od  = jQuery("#"+row+"_customer_order_h_od").val();
+    var thikness  = jQuery("#"+row+"_customer_order_h_thikness").val();
+    var i  = ((od-thikness)*thikness*length*0.02465*quantity)/1000;
     jQuery("#"+row+"_customer_order_total_weight").val(Number(i).toFixed(3));
     jQuery("#"+row+"_customer_order_balance").val(quantity);
   
 }
 
 function total_amount(row){
-    quantity  = jQuery("#"+row+"_customer_order_quantity").val();
-    rate_pcs= jQuery("#"+row+"_customer_order_rate_pcs").val();
-      i  = quantity*rate_pcs;
+    var quantity  = jQuery("#"+row+"_customer_order_quantity").val();
+    var rate_pcs= jQuery("#"+row+"_customer_order_rate_pcs").val();
+      var i  = quantity*rate_pcs;
     jQuery("#"+row+"_customer_order_total_amount").val(Number(i).toFixed(3));
 }
 
 function total_cal_od(){
- height  = jQuery("#"+"serialize_height").val();
-  width  = jQuery("#"+"serialize_width").val();
-    i  = ((width*2.0)+(height*2.0))/3.14;
+  var height  = jQuery("#"+"serialize_height").val();
+  var width  = jQuery("#"+"serialize_width").val();
+   var i  = ((width*2.0)+(height*2.0))/3.14;
  jQuery("#"+"serialize_od").val(Number(i).toFixed(3));
 }
 
 
 
 function cal_bal_quantity(){
- order_qty  = jQuery("#"+"salesplan_order_quantity").val();
-  stock_qty  = jQuery("#"+"salesplan_stock_quantity").val();
-    i  =  order_qty-stock_qty;
+ var order_qty  = jQuery("#"+"salesplan_order_quantity").val();
+  var stock_qty  = jQuery("#"+"salesplan_stock_quantity").val();
+    var i  =  order_qty-stock_qty;
  jQuery("#"+"salesplan_balance_quantity").val(Number(i).toFixed(3));
 }
 
 function cal_bal_wt(){
- order_wt  = jQuery("#"+"salesplan_order_wt").val();
-  stock_wt  = jQuery("#"+"salesplan_stock_wt").val();
-    i  =  order_wt-stock_wt;
+var order_wt  = jQuery("#"+"salesplan_order_wt").val();
+ var stock_wt  = jQuery("#"+"salesplan_stock_wt").val();
+   var i  =  order_wt-stock_wt;
  jQuery("#"+"salesplan_balance_wt").val(Number(i).toFixed(3));
 }
 
@@ -189,32 +189,32 @@ function coildetail(id){
 }
 
 function get_process_coilwt(){
- wt = Number(jQuery('#coil_wt').val());
- p = Number(jQuery('#slitting_division_part').val());
+var wt = Number(jQuery('#coil_wt').val());
+var p = Number(jQuery('#slitting_division_part').val());
     if (p > 0){}else{p = 1;}
    
     jQuery('#slitting_process_coil_wt').val(Number(wt/p).toFixed(3));
 }
 
 function get_slitwt(row){
- pcwt  = jQuery("#slitting_process_coil_wt").val();
- swth  = jQuery("#"+row+"_slitting_slit_width").val();
- cwth  = jQuery("#coil_width").val();
+ var pcwt  = jQuery("#slitting_process_coil_wt").val();
+ var swth  = jQuery("#"+row+"_slitting_slit_width").val();
+ var cwth  = jQuery("#coil_width").val();
  jQuery("#"+row+"_slitting_slit_weight").val(Number((pcwt/cwth)*swth).toFixed(3));
 }
 
 function get_totalwt(row){
- swt  = jQuery("#"+row+"_slitting_slit_weight").val();
- slt  = jQuery("#"+row+"_slitting_no_of_slits").val();
+ var swt  = jQuery("#"+row+"_slitting_slit_weight").val();
+ var slt  = jQuery("#"+row+"_slitting_no_of_slits").val();
  jQuery("#"+row+"_slitting_weight").val(Number(swt*slt).toFixed(3));
 }
 
 function get_scrap(){
- cwth = jQuery("#coil_width").val();
- totalwt = 0;
+ var cwth = jQuery("#coil_width").val();
+ var totalwt = 0;
  for (i=1;i<10;i++){
-     nslit = jQuery("#"+i+"_slitting_no_of_slits").val();
-     swdth = jQuery("#"+i+"_slitting_slit_width").val();
+     var nslit = jQuery("#"+i+"_slitting_no_of_slits").val();
+     var swdth = jQuery("#"+i+"_slitting_slit_width").val();
      totalwt = totalwt + (Number(swdth) * Number(nslit));
  }
  jQuery("#slitting_scrap").val(Number(cwth-totalwt).toFixed(3));
@@ -294,13 +294,13 @@ function stopagedetail(id){
 }
 
 function totaltime(){
-   fromh = jQuery("#tube_mill_stoppage_report_from_4i").val();
-   fromm = jQuery("#tube_mill_stoppage_report_from_5i").val();
-   toh = jQuery("#tube_mill_stoppage_report_to_4i").val();
-   tom = jQuery("#tube_mill_stoppage_report_to_5i").val();   
+   var fromh = jQuery("#tube_mill_stoppage_report_from_4i").val();
+   var fromm = jQuery("#tube_mill_stoppage_report_from_5i").val();
+   var toh = jQuery("#tube_mill_stoppage_report_to_4i").val();
+   var tom = jQuery("#tube_mill_stoppage_report_to_5i").val();   
    
-   from = (Number(fromh)*60)+Number(fromm)
-   to = (Number(toh)*60)+Number(tom)
+   var from = (Number(fromh)*60)+Number(fromm)
+   var to = (Number(toh)*60)+Number(tom)
    
    jQuery("#tube_mill_stoppage_report_total_time").val(Number(to-from)); 
 }
